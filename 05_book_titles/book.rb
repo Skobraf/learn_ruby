1a0fc9fc34450f161869c9386d00a6ae2b96032f
ayoub <skobraf@gmail.com>
2018-06-06 12:50:53 +0000

    added book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
+
 class Book
-# write your code here
+  def title
+    @title
+  end
+  def title=(title)
+    @title = titlieze(title)
+  end
+
+  def titlieze(title)
+    stop_words = %w(and in the of a an)
+    title.capitalize.split.map{|w| stop_words.include?(w) ? w : w.capitalize}.join(' ')
+  end
+
 end
