28bed87fc2acd2168a3190fbf8de85707cda8869
ayoub <skobraf@gmail.com>
2018-06-06 11:37:09 +0000

    simon say solution

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
 #write your code here
+def echo(input)
+  input
+end
+def shout(input)
+  input.upcase!
+end
+def repeat(input, number = 2)
+  number_of_time = ""
+  number.times { number_of_time += input + " "  }
+  return number_of_time.strip!
+end
+def start_of_word(word,number)
+  word[0...number]
+end
+def first_word(input_text)
+  input_text = input_text.split(" ")
+  return input_text[0]
+end
+def titleize(input_text)
+  expet_words = %w(end over and the)
+  input_text.capitalize.gsub(/(\w+)/) do |word|
+    exept_words.include?(word) ? word : word.capitalize
+  end
+end
