f344d7ab96b5918194bf3b4b133f605aa0eb532f
ayoub <skobraf@gmail.com>
2018-06-06 10:59:52 +0000

    passed calculator test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(number_1, number_2)
+  number_1 + number_2
+end
+
+def subtract(number_1, number_2)
+  number_1 - number_2
+end
+
+def sum(input)
+    sum = 0
+  input.each do |x|
+     sum += x
+  end
+  return sum
+end
