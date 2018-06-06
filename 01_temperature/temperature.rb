ca67999da91cc7266db1d16dece00debaaa2c5c9
ayoub <skobraf@gmail.com>
2018-06-06 10:49:39 +0000

    passed fehranait test

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc(number_input)
+  celsius = (number_input - 32) / 1.8
+  return celsius.round
+end
+
+def ctof(number_input)
+  fahrenheit = (number_input) * 1.8 + 32
+end
